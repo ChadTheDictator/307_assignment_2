3e443d911815d173aa43b1cec389d2a2f84717af
N Mraz <khuliloach53@gmail.com>
2026-03-24 01:06:33 -0600

    24/03

--- Cubic Spline.py
+++ Cubic Spline.py
@@ -13,8 +13,74 @@ def read_csv(file):
             sh.append(float(SH))
     return sv, sh
 
-def spline(sv, sh, 
+def S(x_value, x, h):
+    interval_num = 0
+    for i in range(n):
+        if x[i] <= x_value <= x[i+1]:
+            interval_num = i
+            break
+    
+    hi = h[i]
+
+    #term_1 = 
+    return
+
+def spline(file):
+    sv, sh = read_csv(file)
+    n = len(sv)-1
+
+    z=[]
+
+    h = []
+    u = []
+    v = []
+    b = []
+    y = sh
+    x = sv
+    A = []
+    for i in range(n):
+        h += [(x[i+1] - x[i])]
+    
+    for i in range(n):
+        b += [(y[i+1]-y[i])/h[i]]
+    
+    for i in range(1,n):
+        u += [2*(h[i-1] + h[i])]
+        v += [6*(b[i]-b[i-1])]
+
+    for i in range(n-1):
+        row = [0]*(n-1)
+
+        # Main diagonal
+        row[i] = u[i]
+
+        # Lower diagonal
+        if i > 0:
+            row[i-1] = h[i]
+
+        # Upper diagonal
+        if i < n-2:
+            row[i+1] = h[i+1]
+
+        A.append(row)
+
+       
+        v.append(6*(b[i+1] - b[i]))
+
+    return A, v
+    #print(h,u,b,v,y,x)
+   # v+=[h[i-1]*z[i-1]+u[i]*z[i]+h[i]*z[i+1]]
 sv, sh = read_csv(file)
+n = len(sv)-1
+    
+A,v = spline(file)
+#vars = []
+#for i in range(n):
+#    vars+=["z"]
+
+z = gauss_elim_p_piv(A,v,vars)
+
+print(z)
 
 
 #print(sv)
